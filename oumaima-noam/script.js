332ec4ad9be90792536824f114961a794da8bb63
RoyalDigitalInvite <royaldigitalinvite@gmail.com>
2026-08-18 15:16:24 +0100

    oumaima-noam

--- oumaima-noam/script.js
+++ oumaima-noam/script.js
@@ -7,7 +7,7 @@ const musicIcon = document.getElementById("musicIcon");
 
 
 const WEDDING_CONFIG = {
-  whatsappNumber: "212641094714",
+  whatsappNumber: "33749929286",
 
   brideName: "oumaïma Layouni",
   groomName: "Noam Khedhiri",
